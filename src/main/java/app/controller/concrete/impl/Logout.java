05cdcd9902583afab53dc798a9f37d7174c2435a
ElenPavliychenkova <e.pavliychenkova@gmail.com>
2024-11-20 17:03:51 +0100

    added dao/service layer

---
+++ src/main/java/app/controller/concrete/impl/Logout.java
@@ -0,0 +1,22 @@
+package app.controller.concrete.impl;
+
+import app.controller.concrete.Command;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Logout implements Command {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate();
+        }
+
+
+        response.sendRedirect("Controller?command=go_to_index_page");
+    }
+}
+
